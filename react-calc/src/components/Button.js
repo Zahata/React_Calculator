b092f0d6ac3ad061f215577f3ec48dacfb086992
Zahata <253925@via.dk>
2023-03-03 16:52:50 +0200

    calculator done, could have some improvements in future

--- react-calc/src/components/Button.js
+++ react-calc/src/components/Button.js
@@ -17,15 +17,94 @@ const Button = ({value}) => {
   const commaClick = () => {
     setCalc({
         ...calc,
-        num: !calc.num.toSring().includes('.') ? calc.num + value : calc.num
+        num: !calc.num.toString().includes('.') ? calc.num + value : calc.num
+    })
+  }
+
+  const resetClick = () => {
+    setCalc({
+      sign: '',
+      num: 0,
+      res: 0
+    });
+  }
+
+  const handleNumberClick = () => {
+    const numberString = value.toString();
+    let numberValue;
+
+    if(numberString === '0' && calc.num === 0){
+      numberValue = '0';
+    } else {
+      numberValue = Number(calc.num + numberString);
+    }
+
+    setCalc({
+      ...calc,
+      num: numberValue
+    })
+  }
+
+  const signClick = () => {
+    setCalc({
+      sign: value,
+      res: !calc.res && calc.num ? calc.num : calc.res,
+      num: 0
+    })
+  }
+
+  const equalsClick = () => {
+    if(calc.res && calc.num !== 0){
+      const doMath = (a, b, sign) => {
+        const result = {
+          '+': (a,b) => a+b, 
+          '-': (a,b) => a-b,
+          'x': (a,b) => a*b,
+          '/': (a,b) => a/b,
+        }
+        return result[sign](a,b);
+      }
+      setCalc({
+        res: doMath(calc.res, calc.num, calc.sign),
+        sign: '',
+        num: 0
+      })
+    }
+  }
+
+  const percentClick = () => {
+    setCalc({
+      num: (calc.num / 100),
+      res: calc.res / 100,
+      sign: ''
+    })
+  }
+
+  const invertClick = () => {
+    setCalc({
+      num: calc.num ? calc.num * -1 : 0,
+      res: calc.res ? calc.num * -1 : 0,
+      sign: ''
     })
   }
 
   const handleBtnClick = () => {
     const results = {
-        '.': commaClick
+        '.': commaClick,
+        'C': resetClick,
+        '/': signClick,
+        'x': signClick,
+        '+': signClick,
+        '-': signClick,
+        '=': equalsClick,
+        '%': percentClick,
+        '+-': invertClick
     }
-    return results[value]()
+    if(results[value]){
+      return results[value]()
+    } else {
+      return handleNumberClick();
+    }
   }  
   return (
     <button onClick={handleBtnClick} className={`${getStyleName(value)} btn`}>{value}</button>
