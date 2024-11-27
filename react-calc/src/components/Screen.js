7aece2808c51c49d1d9376bef04237810fdb9ad3
Zahata <253925@via.dk>
2024-11-27 16:57:05 +0200

    updates some dependencies and readme

--- react-calc/src/components/Screen.js
+++ react-calc/src/components/Screen.js
@@ -1,6 +1,6 @@
 import React, { useContext } from 'react'
 import { CalcContext } from '../context/CalcContext'
-import { Textfit } from 'react-textfit';
+import { Textfit } from '@namhong2001/react-textfit';
 
 const Screen = () => {
   const { calc } = useContext(CalcContext);
